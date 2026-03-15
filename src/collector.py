6eb4ad0f80df7d79304984634b94c25512c69a2f
arretee <arreteear@gmail.com>
2026-03-15 21:15:17 +0200

    Added an argument to function get_disc_usage. if the function used without argument, it returns Data of C disc.

--- src/collector.py
+++ src/collector.py
@@ -1,4 +1,5 @@
 import psutil
+import os
 
 
 def get_cpu_percent():
@@ -7,8 +8,14 @@ def get_cpu_percent():
 def get_virtual_memory():
     return psutil.virtual_memory()
 
-def get_disc_usage():
-    return psutil.disk_usage('/')
+def get_disc_usage(disc = 'C:/'):
+    """
+    Returns named tuple with data about disc, if used without argument returns data about disc C.
+
+    :param disc: gets string with name of the disc. example of string: 'C:/'
+    :return: Returns named tuple with data about disc, example of return: sdiskusage(total=999495213056, used=336265076736, free=663230136320, percent=33.6)
+    """
+    return psutil.disk_usage(disc)
 
 def get_disk_partitions():
     return psutil.disk_partitions()
