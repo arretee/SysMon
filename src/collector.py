8b36c94de8af3aeb99e387f3267bc98a3f7343dc
arretee <arreteear@gmail.com>
2026-03-26 11:18:03 +0200

    Created 3 Functions to get Network data in Collector
    get_network_speed_deque_for_every_pc_connection
    
    get_network_connection_speed_deque
    
    network_dl_ul_calculation

--- src/collector.py
+++ src/collector.py
@@ -1,5 +1,8 @@
 import datetime
+import threading
+import time
 from time import sleep
+from collections import deque
 
 import psutil
 
@@ -80,6 +83,84 @@ def get_discs_usage_percent():
         disks_usage_percent[disc] = round(get_disc_usage(disc)[3], 1)
 
     return disks_usage_percent
+
+# -------------------------------- NetWork Data functions --------------------------------
+def get_network_speed_deque_for_every_pc_connection(interval):
+    """
+    Function that creates dict with all pc network connection keys, and refreshable deque's like values that shows network speed for that connection
+
+
+    :param interval: Number of seconds between every deque is refreshed the info.
+    :return: example of dict: {"Ethernet": deque(), "WiFi": deque()}, each deque refresh every (interval) seconds by itself and store current speed of network on this connection.
+    """
+    # Get all network connections
+    counter = psutil.net_io_counters(pernic=True)
+    connections_list = []
+    for key in counter.keys():
+        connections_list.append(key)
+
+    # Create final dict with deque's
+    connections_speed = {}
+    for connection in connections_list:
+        connections_speed[connection] = get_network_connection_speed_deque(interval, connection)
+
+    return connections_speed
+
+def get_network_connection_speed_deque(interval, interface="Ethernet"):
+    """
+    Function creates deque that stores relevant network data in format like -> deque[-1] = (current dl, current ul).
+
+    :param interval: Interval that data refreshes inside the deque.
+    :return: deque with data, data refresh every (Interval) of seconds.
+    :param interface: PC interface to check speed on, for example -> "Ethernet" (By Default is "Ethernet")
+    """
+
+    # Create Deque for thread results
+    dl_ul_speed_deque = deque(maxlen=1)
+
+    # Create thread for collecting network data
+    thread_network_calculation = threading.Thread(target=network_dl_ul_calculation, args=(dl_ul_speed_deque, interval, interface))
+    thread_network_calculation.daemon = True
+
+    thread_network_calculation.start()
+
+
+    return dl_ul_speed_deque
+
+def network_dl_ul_calculation(q, interval, interface="Ethernet"):
+    """
+    Function creates While loop that endlessly calculate network speed information and store it inside deque.
+    Use function like a thread that will run endlessly.
+    calculate speed in units of kB/s.
+
+    :param q: deque to store info about networking, current data stored in last element like (dl, du)
+    :param interval: interval between calculations
+    :param interface: PC interface to check speed on, for example -> "Ethernet" (By Default is "Ethernet")
+    :return: None
+    """
+    time_start = time.time()
+
+    counter = psutil.net_io_counters(pernic=True)[interface]
+    stats =  (counter.bytes_recv, counter.bytes_sent)
+
+    while True:
+        # Save current stats
+        last_stats = stats
+
+        # Sleep for interval
+        time.sleep(interval)
+
+        # Get new stats
+        counter = psutil.net_io_counters(pernic=True)[interface]
+        time_current = time.time()
+        stats = (counter.bytes_recv, counter.bytes_sent)
+
+        dl = (stats[0] - last_stats[0]) / (time_current - time_start) / 1000
+        ul = (stats[1] - last_stats[1]) / (time_current - time_start) / 1000
+
+        q.append((dl, ul))
+        time_start = time.time()
+
 
 # -------------------------------- Get Time Functions --------------------------------
 def get_time_string():
